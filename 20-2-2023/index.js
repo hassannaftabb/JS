997b1ee760a1819246c27d56e8208fb5782faf1d
Hassan Aftab <hassannaftabb@gmail.com>
2023-02-20 20:03:06 +0500

    first commit

---
+++ 20-2-2023/index.js
@@ -0,0 +1,33 @@
+var valueOne = 12;
+var valueTwo = 15;
+
+//USING STRING LITERALS
+var str = `The value of the variable "valueOne" is: ${valueOne}`;
+console.log("Variable value added:", str);
+
+//TYPES OF OPERATORS IN JAVASCRIPT
+//ARITHMETIC OPERATORS
+
+//Addition operator (+)
+var sum = valueOne + valueTwo;
+console.log("The sum of valueOne and valueTwo is:", sum);
+
+//Multiplication Operator (*)
+var product = valueOne * valueTwo;
+console.log("The product of valueOne and valueTwo is:", product);
+
+//Division Operator (/)
+var qoutient = valueOne / valueTwo;
+console.log("The qoutient of division of valueOne by valueTwo", qoutient);
+
+//Difference operator (-)
+var difference = valueTwo - valueOne;
+console.log("The difference of valueOne from valueTwo is:", difference);
+
+//Modulus Operator (%)
+var modulus = 30 % 15;
+console.log("The remainder after dividing valueOne with valueTwo is:", modulus);
+
+//Exponential Operator (**)
+var answer = 3 ** 3;
+console.log("The power of 3 to 3  is:", answer);
