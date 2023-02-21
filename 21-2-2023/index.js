2c461654138dddc15b0cbf4f30322a1119692810
Hassan Aftab <hassannaftabb@gmail.com>
2023-02-21 16:18:30 +0500

    Assignment and Relational Operators

---
+++ 21-2-2023/index.js
@@ -0,0 +1,69 @@
+//ASSIGNMENT OPERATORS
+
+//Simple Assignment Operator
+let number = 10;
+
+//Addition Assignment Operator
+let x = 5;
+x += 10; //x = x + 10
+console.log("The value of x after adding its value with 10 is:", x);
+
+//Subtraction Assignment Operator
+let y = 5;
+y -= 10; //y = y - 10
+console.log("The value of y after subtracting its value with 10 is:", y);
+
+//Division Assignment Operator
+let z = 5;
+z /= 10; //z = z / 10
+console.log("The value of z after dividing its value with 10 is:", z);
+
+//Multiplication Assignment Operator
+let a = 5;
+a *= 10; //a = a * 10
+console.log("The value of a after multiplying its value with 10 is:", a);
+
+//Multiplication Assignment Operator
+let b = 5;
+b %= 10; //b = b % 10
+console.log("The value of b after taking modulus of its value with 10 is:", b);
+
+//Multiplication Assignment Operator
+let c = 5;
+c **= 10; //c = c ** 10
+console.log("The value of c raise to power 10 is:", c);
+
+//RELATIONAL OPERATORS
+
+//Equals To (==)
+console.log("5 is equals to 5?", 5 == "5");
+console.log("5.5 is equals to 5.5?", "5.5" == 5.5);
+console.log("Two strings are equal?", "hassan" == "hassan");
+
+//Identity Equals To (===)
+console.log("5 is strictly equals to 5?", 5 === "5");
+console.log("5.5 is strictly equals to 5.5?", "5.5" === 5.5);
+console.log("Two strings are strictly equal?", "hassan" === "hassan");
+
+//Not Equals to (!=)
+
+console.log("5 is not equals to 5:", "5" != 5);
+console.log("5.5 is not equals to 5.5:", 5.5 != 5.5);
+console.log("two strings are not equal:", "hassan" != "hassan");
+
+//Identity Not Equals to (!==)
+console.log("5 is not equals to 5:", "5" !== 5);
+console.log("5.5 is not equals to 5.5:", 5.5 !== 5.5);
+console.log("two strings are not equal:", "hassan" !== "hassan");
+
+//Greater Than (>)
+console.log("6 is greater than 5:", 6 > 5);
+
+//Greater than equals to (>=)
+console.log("6 is greater or equal to 5:", 6 >= 5);
+
+//Less Than (<)
+console.log("6 is less than 5:", 6 < 5);
+
+//Less than equals to (<=)
+console.log("6 is less or equal to 5:", 6 <= 5);
