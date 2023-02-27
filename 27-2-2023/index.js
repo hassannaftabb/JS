e587c9ed5b3362d4c7e79e0d29003e8783e19514
Hassan Aftab <hassannaftabb@gmail.com>
2023-02-27 16:47:25 +0500

    function done

---
+++ 27-2-2023/index.js
@@ -0,0 +1,31 @@
+//FUNCTIONS IN JAVASCRIPT
+
+//greet function
+function greet() {
+  console.log("Assalamualaikum");
+}
+
+//function call (calling a function)
+greet("Hassan");
+greet("Mahnoor");
+
+//sum function
+function sum(a, b) {
+  console.log("The sum of these number is", a + b);
+}
+
+//sum function call
+sum(12, 35, 78);
+sum(1000000, 382947823);
+
+//custom function
+function isGreaterThan30(num) {
+  if (num * 10 > 30) {
+    console.log("Hellow");
+  } else {
+    console.log("Bye!");
+  }
+}
+
+//calling custom fucntion
+isGreaterThan30(4);
