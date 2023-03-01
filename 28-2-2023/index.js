4eca8384b647636bc212a7b88f330875ce9b20cd
Hassan Aftab <hassannaftabb@gmail.com>
2023-03-01 16:19:23 +0500

    yesterday string methods

---
+++ 28-2-2023/index.js
@@ -0,0 +1,27 @@
+//STRINGS
+
+var str = `hello 123@#$-= jfgujg`;
+
+var str2 = `stauseefr`;
+
+// console.log(str + str2 + "ifdugjhyfbghfybf" + 2);
+// console.log(str.length);
+
+// console.log(str2.concat("890"));
+
+// console.log(str2.charAt(0));
+
+// console.log(str2.endsWith("r"));
+
+// console.log(str2.includes("t auseef"));
+
+// console.log(str2.indexOf("u"));
+
+// console.log(str2.replace("au", "bl"));
+
+// console.log(str2.slice(1, 5));
+
+//homework
+// 1- .substring
+// 2- .repeat
+// 3- .startWith
