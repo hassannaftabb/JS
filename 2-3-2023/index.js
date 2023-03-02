2f342000e9177ef5ec5f32b0ed742974aa3f2ee0
Hassan Aftab <hassannaftabb@gmail.com>
2023-03-03 00:01:06 +0500

    2 days work

---
+++ 2-3-2023/index.js
@@ -0,0 +1,14 @@
+const myNumber = 20982098371;
+
+//isNaN
+// console.log(isNaN("89"));
+
+//parseInt
+// console.log(parseInt("20.2"));
+
+//parseFloat
+// console.log(parseFloat(20));
+
+//toString or toLocaleString
+// console.log(myNumber.toLocaleString());
+// console.log(myNumber.toString());
